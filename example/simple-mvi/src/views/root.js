4ec694c094eb4a192d01479b9fe49b90eecf26d7
Takashi Nakagawa <tak1240@gmail.com>
2015-10-17 22:46:38 +0900

    update

--- example/simple-mvi/src/views/root.js
+++ example/simple-mvi/src/views/root.js
@@ -1,11 +1,11 @@
 /** @jsx hJSX */
 import {hJSX} from '@cycle/dom';
 
-export default function root({content}){
+export default function root(...args){
   return (
     <div>
       <textarea name="raw" id="raw" cols="30" rows="10"></textarea>
-      <div id="markdown">{content}</div>
+      <div id="markdown">{args[0][1]}</div>
     </div>
   );
 }
--- src/make-markdown-driver.js
+++ src/make-markdown-driver.js
@@ -4,7 +4,7 @@ import marked from 'marked';
 // https://github.com/vic/awesome-cyclejs/pull/8#issuecomment-148759962
 export default function makeMarkdownDriver(renderer = marked) {
   return function markdownDriver(request$) {
-    return request$.flatMap(raw => {
+    return request$.map(raw => {
       return Rx.Observable.fromCallback(renderer)(raw);
     })
   }
